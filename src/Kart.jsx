2ab874e1c9331ecd3f2435acfb8a8441987eac54
Abhishek <abhishek.kabi.21@gmail.com>
2025-08-17 12:14:16 +0530

    Added new feature XYZ

--- src/Kart.jsx
+++ src/Kart.jsx
@@ -13,38 +13,42 @@ function Kart({ cart }) {
     <>
       <Navbar />
       {console.log(cart)}
-
-      <div className="kartitemwrapper">
-        {cart.map((item, i) => (
-          <div key={i} className="kartitems">
-            <img
-              src={item.imageSource}
-              alt=""
-              style={{ height: "40px", width: "60px" }}
-            />
-            <h3>{item.name}</h3>
-            <p>
-              ₹{item.price} × {item.quantity}
-            </p>
-            <p>Subtotal: ₹{item.price * item.quantity}</p>
+      {cart.length === 0 ? (
+        <p style={{ textAlign: "center", marginTop: "80px" }}>
+          Your cart is empty
+        </p>
+      ) : (
+        <div className="kartitemwrapper">
+          {cart.map((item, i) => (
+            <div key={i} className="kartitems">
+              <img
+                src={item.imageSource}
+                alt=""
+                style={{ height: "40px", width: "60px" }}
+              />
+              <h3>{item.name}</h3>
+              <p>
+                ₹{item.price} × {item.quantity}
+              </p>
+              <p>Subtotal: ₹{item.price * item.quantity}</p>
+            </div>
+          ))}
+          <div className="totaldiv">
+            <h3>Total = {totalPrice}</h3>
           </div>
-        ))}
-        <div className="totaldiv">
-          <h3>Total = {totalPrice}</h3>
-        </div>
 
-        <div className="placeorderbuttondiv">
-          <button className="placeorderbutton">Place order</button>
+          <div className="placeorderbuttondiv">
+            <button className="placeorderbutton">Place order</button>
+          </div>
         </div>
-
-        <div className="continueshoppingbutton">
-          <button
-            className="continueshopping-button"
-            onClick={() => navigate(-1)}
-          >
-            Continue Shopping
-          </button>
-        </div>
+      )}
+      <div className="continueshoppingbutton">
+        <button
+          className="continueshopping-button"
+          onClick={() => navigate(-1)}
+        >
+          Continue Shopping
+        </button>
       </div>
     </>
   );
